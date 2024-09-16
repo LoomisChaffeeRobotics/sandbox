27ed8263111595a5e88efb202a8730493202c30b
KDloomis <NaDonkor@internal.loomis.org>
2024-09-16 17:24:53 -0400

    Added Setpower

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sandbox/Day02ButtonsAndTankDrive/Opmode.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.sandbox.Day02ButtonsAndTankDrive;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class Opmode extends OpMode {
+DcMotor frontLeft;
+DcMotor frontRight;
+DcMotor rearLeft;
+DcMotor rearRight;
+
+    @Override
+    public void init() {
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        rearLeft = hardwareMap.get(DcMotor.class, "rearleft");
+        rearRight = hardwareMap.get(DcMotor.class, "rearRight");
+    }
+
+
+    @Override
+    public void loop() {
+
+            frontLeft.setPower(gamepad1.left_stick_y);
+            frontRight.setPower(gamepad1.right_stick_y);
+            rearLeft.setPower(gamepad1.right_stick_y;
+            rearRight.setPower(gamepad1.right_stick_y);
+
+
+
+    }
+}
