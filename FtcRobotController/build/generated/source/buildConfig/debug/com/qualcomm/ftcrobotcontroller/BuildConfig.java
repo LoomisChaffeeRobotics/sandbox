9ef30bfad895faf33a094a33c928d95e90665fd2
Millen-LC <millen_berglind@loomis.org>
2024-09-16 17:35:19 -0400

    .

---
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.qualcomm.ftcrobotcontroller;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String APP_BUILD_TIME = "2024-09-16T17:31:04.254-0400";
+}
