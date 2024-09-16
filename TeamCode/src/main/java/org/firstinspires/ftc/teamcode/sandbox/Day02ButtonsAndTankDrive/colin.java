89ad04dafa106510a9579983f4476f93d1bff9df
violin2024 <musichu20171819@gmail.com>
2024-09-16 17:24:48 -0400

    Colin Tankdrive1
    added motors, setPower

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sandbox/Day02ButtonsAndTankDrive/colin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sandbox/Day02ButtonsAndTankDrive/colin.java
@@ -1,2 +1,27 @@
-package org.firstinspires.ftc.teamcode.sandbox.Day02ButtonsAndTankDrive;public class colin {
+package org.firstinspires.ftc.teamcode.sandbox.Day02ButtonsAndTankDrive;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class colin extends OpMode {
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor rearLeft;
+    DcMotor rearRight;
+    @Override
+    public void init() {
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        rearLeft = hardwareMap.get(DcMotor.class, "rearLeft");
+        rearRight = hardwareMap.get(DcMotor.class, "rearRight");
+    }
+
+    @Override
+    public void loop() {
+        frontLeft.setPower(gamepad1.left_stick_y);
+        rearLeft.setPower(gamepad1.left_stick_y);
+        frontRight.setPower(gamepad1.right_stick_y);
+        rearRight.setPower(gamepad1.right_stick_y);
+
+    }
 }
