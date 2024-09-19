e2ce783e34fa003ef661f930b3afc17a21edfd14
Millen-LC <millen_berglind@loomis.org>
2024-09-19 15:43:11 -0400

    Update millen.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sandbox/Day03IntakeServosSensors/millen.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sandbox/Day03IntakeServosSensors/millen.java
@@ -4,18 +4,23 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cColorSensor;
+
 @TeleOp
 public class millen extends OpMode {
 
-
+    ModernRoboticsI2cColorSensor CSensor;
 
     @Override
     public void init() {
-
+        CSensor = hardwareMap.get(ModernRoboticsI2cColorSensor.class, "ColorSensor");
     }
 
     @Override
     public void loop() {
+        if (CSensor.blue() > CSensor.green() && CSensor.blue() > CSensor.red()) {
+
+        }
 
     }
 }
