68d20bf3f1baa71f170c44f3249b8439bc115648
Millen-LC <millen_berglind@loomis.org>
2024-09-20 17:16:28 -0400

    Update millen_pid.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sandbox/Day03IntakeServosSensors/millen_pid.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sandbox/Day03IntakeServosSensors/millen_pid.java
@@ -4,20 +4,36 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Config
 @TeleOp
 public class millen_pid extends OpMode{
 
     DcMotor motor1;
-
+    public static double Kp = 0;
+    public static double Ki = 0;
+    double lastError = 0;
+    double integralSum = 0;
+    int TargetPos = 500;
+    ElapsedTime timer;
+    
     @Override
     public void init() {
         motor1 = hardwareMap.get(DcMotor.class, "motor1");
+        timer = new ElapsedTime();
     }
 
     @Override
     public void loop() {
-
+        int encoderPosition = motor1.getCurrentPosition();
+        int error = TargetPos - encoderPosition;
+        double derivative = ((error - lastError) / timer.seconds());
+        integralSum += (error * timer.seconds());
+        double out = ((Kp * error) + (Ki * integralSum));
+        motor1.setPower(out);
+        lastError = error;
+        timer.reset();
+        
     }
 }
