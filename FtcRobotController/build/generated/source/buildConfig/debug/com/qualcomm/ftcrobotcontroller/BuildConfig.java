3a27e8759243a5b874f6481755f546404ee6c1b2
Dylanqw <145394726+Dylanqw@users.noreply.github.com>
2024-10-01 16:31:09 -0400

    constants and more

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2024-10-01T15:35:06.854-0400";
+  public static final String APP_BUILD_TIME = "2024-10-01T16:29:44.637-0400";
 }
