d4d975bbc2afc255ae8bc41c62fa9c6eef234c26
violin2024 <musichu20171819@gmail.com>
2024-10-08 16:21:47 -0400

    Complete fieldcentric drive!!!!!
    YAY - changed drive so it works this time, allowed both gamepads to hit start button to reset yaw.

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2024-10-08T11:15:13.966-0400";
+  public static final String APP_BUILD_TIME = "2024-10-08T16:09:10.423-0400";
 }
