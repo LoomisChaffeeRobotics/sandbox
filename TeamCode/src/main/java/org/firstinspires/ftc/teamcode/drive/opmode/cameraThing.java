d89502790b3b0dcbc64924eb31ffdb768afee444
Dylanqw <145394726+Dylanqw@users.noreply.github.com>
2024-10-17 16:25:23 -0400

    camera thing
    loigfdcfgui

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/cameraThing.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+
+@TeleOp
+public class cameraThing extends OpMode {
+
+    IMU imu;
+    AprilTagProcessor myAprilTagProcessor;
+    VisionPortal myVisionPortal;
+    SampleMecanumDrive drive;
+
+    @Override
+    public void init() {
+        drive  = new SampleMecanumDrive(hardwareMap);
+
+        imu = hardwareMap.get(IMU.class, "imu");
+// Create the AprilTag processor and assign it to a variable.
+        myAprilTagProcessor = AprilTagProcessor.easyCreateWithDefaults();
+        // Enable or disable the AprilTag processor.
+        myVisionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), myAprilTagProcessor);
+
+    }
+
+    @Override
+    public void loop() {
+        aprilTagTelemetry();
+        telemetry.addData("pose",drive.getPoseEstimate());
+        drive.update();
+    }
+    private void aprilTagTelemetry() {
+        List<AprilTagDetection> currentDetections = myAprilTagProcessor.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+
+            // targetTag isolated
+
+            telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+            telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+            telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+            telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+
+
+        }
+
+
+    }
+}
