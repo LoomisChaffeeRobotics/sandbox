68fa7653b121134eeb7e736d0995d51dae1311dd
violin2024 <musichu20171819@gmail.com>
2024-10-22 17:27:15 -0400

    Added something such that something works maybe :)

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2024-10-22T16:44:25.949-0400";
+  public static final String APP_BUILD_TIME = "2024-10-22T17:25:30.988-0400";
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/CameraCalibrationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/CameraCalibrationTest.java
@@ -56,7 +56,7 @@ import java.util.Locale;
  * In OnBot Java, use "Add File" to add this OpMode from the list of Samples.
  */
 
-@TeleOp(name = "Utility: Camera Frame Capture", group = "Utility")
+@TeleOp
 
 public class CameraCalibrationTest extends LinearOpMode
 {
